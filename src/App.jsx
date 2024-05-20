141e7fe7867d89245689ab2ccf29b792edd4f4d2
Jaffar Sharieff <jaffar1187@gmail.com>
2024-05-20 20:09:40 +0530

    intial setup && sidebar implementation

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from './components/SidebarComponents/Sidebar'
+
+const App = () => {
+  return (
+   <>
+    <Sidebar />
+   </>
+  )
+}
+
+export default App
